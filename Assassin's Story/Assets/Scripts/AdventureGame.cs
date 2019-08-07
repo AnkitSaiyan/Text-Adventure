ef9417d8cf90733cdfac5842f3608c92f4cd5195
Ankit Kumar <ankitkumar36905@gmail.com>
2019-08-08 04:16:37 +0530

    Project Files added

---
+++ Assassin's Story/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+    int flag = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        
+        var nextStates = state.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.S) && flag == 0)
+        {
+            state = nextStates[0];
+            flag = 1;
+        }
+        /*else if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q))
+        {
+            
+        }*/
+        // Getting Key using for loop
+        else if(flag != 0)
+        {
+            for (int i = 0; i < nextStates.Length; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                {
+                    state = nextStates[i];
+                }
+            }
+        }
+        
+
+
+    /*  else if(flag!=0)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                state = nextStates[0];
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                state = nextStates[1];
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                state = nextStates[2];
+            }
+        }  */
+        
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ Assassin's Story/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "State")]
+
+public class State : ScriptableObject
+{
+    [TextArea(10,12)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
