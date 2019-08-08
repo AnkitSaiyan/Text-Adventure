f7cede5dc5777d7a76426804c72ca94e64407a01
Ankit Kumar <ankitkumar36905@gmail.com>
2019-08-08 13:02:44 +0530

    Updated Project Files
    Game Quit Functionality implemented.

--- Assassin's Story/Assets/Scripts/AdventureGame.cs
+++ Assassin's Story/Assets/Scripts/AdventureGame.cs
@@ -1,25 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;  // To use class MonoBehavior Functionalities
-using TMPro;    // To use TextMeshPro field
+using UnityEngine;
+using TMPro;
 
 public class AdventureGame : MonoBehaviour
 {
-    // SerializeFiled used to make it appear in Inspector
     [SerializeField] TextMeshProUGUI textComponent;
     [SerializeField] State startingState;
-    
-    // state is an object of class State
+
     State state;
     int flag = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
         state = startingState;
-        // GetStateStory function called which returns Story text of the current state and
-        // put it in the Text field of Story Text element of Canvas
         textComponent.text = state.GetStateStory();
         
     }
@@ -32,7 +27,7 @@ public class AdventureGame : MonoBehaviour
 
     private void ManageState()
     {
-        //Copying the whole array of nextstates into nextStates
+        
         var nextStates = state.GetNextStates();
 
         if (Input.GetKeyDown(KeyCode.S) && flag == 0)
@@ -40,10 +35,10 @@ public class AdventureGame : MonoBehaviour
             state = nextStates[0];
             flag = 1;
         }
-        /*else if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q))
+        else if(Input.GetKeyDown(KeyCode.Escape))
         {
-            
-        }*/
+            Application.Quit();
+        }
         // Getting Key using for loop
         else if(flag != 0)
         {
